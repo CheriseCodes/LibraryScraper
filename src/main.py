dd577306992b1b6983ac84e8ae86431c402a8f34
CheriseCodes <cherise.bry@gmail.com>
2021-09-23 19:26:32 -0400

    Update main.py so it works with new interface

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-from selenium.webdriver.support.expected_conditions import element_selection_state_to_be
-import sys
-from library import *
+from selenium import webdriver
+from libscrape.library import *
 import subprocess
 import os
 
@@ -8,9 +7,10 @@ import os
 
 try:
     #print(sys.argv[1])
+    login_info = {"p":(os.environ['PPL_USER'], os.environ['PPL_PASS']), "w":(os.environ['WPL_USER'], os.environ['WPL_PASS']), "t":(os.environ['TPL_USER'],os.environ['TPL_PASS'])}
     driver = webdriver.Chrome("chromedriver")
     library_obj = TPL(driver)
-    on_hold = library_obj.get_items_on_hold()
+    on_hold = library_obj.items_on_hold(login_info['t'][0],login_info['t'][1])
     if on_hold:
         for item in on_hold:
             print(item)
