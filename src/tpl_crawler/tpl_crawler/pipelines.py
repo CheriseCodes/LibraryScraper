ab396f6865db8831c0d01cd483551517a55be8c5
CheriseCodes <cherise.bry@gmail.com>
2022-02-08 21:36:46 -0500

    Store crawled data in a relational database

--- src/tpl_crawler/tpl_crawler/pipelines.py
+++ src/tpl_crawler/tpl_crawler/pipelines.py
@@ -2,12 +2,28 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import sqlite3
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
 class TplCrawlerPipeline:
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+    def open_spider(self, spider):
+        self.conn = sqlite3.connect('tpl_crawler.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Books(book_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT,
+        contributors TEXT, branches TEXT)''') #TODO: Update branches to JSON
+
+    def close_spider(self, spider):
+        self.cursor.execute('''SELECT * FROM Books''')
+        print(self.cursor.fetchall())
+        self.conn.commit()
+        self.conn.close()
     def process_item(self, item, spider):
+        print(item['title'])
+        self.cursor.execute('''INSERT INTO Books(title, contributors, branches) VALUES (?,?,?)''', (item['title'], item['contributors'], str(item['branches'])))
         return item
--- src/tpl_crawler/tpl_crawler/spiders/tpl.py
+++ src/tpl_crawler/tpl_crawler/spiders/tpl.py
@@ -4,6 +4,7 @@ from scrapy.linkextractors import LinkExtractor
 import re
 
 class TplSpider(CrawlSpider):
+    # TODO: Copy this pagination pattern: https://docs.scrapy.org/en/latest/intro/tutorial.html?highlight=pagination#more-examples-and-patterns
     name = 'tpl'
     allowed_domains = ['torontopubliclibrary.ca']
     start_urls = ['https://www.torontopubliclibrary.ca/search.jsp?N=37918+4294952073+37844+20206+37751&Ntt=Python+(Computer+program+language)']
@@ -12,15 +13,15 @@ class TplSpider(CrawlSpider):
     }
     rules = [Rule(LinkExtractor(allow=r'https:\/\/www\.torontopubliclibrary\.ca\/detail\.jsp\?Entt='), callback='parse_info', follow=True)]
     def parse_info(self, response):
+        #print(response.text)
         title = response.xpath('//*[@id="bib-detail"]/div[1]/div[2]/div[1]/h1/text()').get()
         contributors = response.xpath('//*[@id="bib-detail"]/div[1]/div[2]/div[3]/a/text()').get()
         branches = response.css('#item-availability tr td > b > a::text').getall()
         branches = [re.sub('\s+', ' ', branch).strip() for branch in branches if (('Closed' not in branch) and ('Toronto Public Library' not in branch))]
         page_url = response.url
-        return {
+        yield {
             "title": title.strip(),
             "url": page_url,
             "contributors": contributors,
             "branches": branches
         }
-        #print(title, contributors, branches)
